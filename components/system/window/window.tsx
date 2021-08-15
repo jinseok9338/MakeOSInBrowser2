e1616b272d2869a3c3619a848269aeea77a05b8d
jinseok9338 <jinseok9338@gmail.com>
2021-08-15 13:35:01 +0000

    Make draggable window with close Functionality

--- components/system/window/window.tsx
+++ components/system/window/window.tsx
@@ -1,6 +1,9 @@
-import { Box, createStyles, makeStyles, Theme } from "@material-ui/core";
+import { AppBar, Container, createStyles, makeStyles, Theme } from "@material-ui/core";
 import React from "react";
 import Draggable from "react-draggable";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
+import { ProcessConsumer } from "contexts/process";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -10,21 +13,40 @@ const useStyles = makeStyles((theme: Theme) =>
       position: "absolute",
       textAlign: "center",
       fontSize: "50px",
-      cursor: "grab"
+      paddingLeft: 0,
+      paddingRight: 0,
+      width: "350px"
     },
     handle: {
-      cursor: "grab"
+      cursor: "grab",
+      height: "30px"
+    },
+    icon:{
+      fontSize: "25px",
+      top:"2px",
+      right: "4px",
+      position: "absolute",
+      cursor: "pointer",
     }
   })
 );
 
-const Window = ({ children }: { children: React.ReactNode }): JSX.Element => {
+const Window = ({ children }: { children: React.ReactChild  }): JSX.Element => {
   const classes = useStyles();
 
   return (
-    <Draggable>
-      <Box className={classes.windowBox}>{children}</Box>
-    </Draggable>
+    <ProcessConsumer>{
+      ({open,close }) => (
+        <Draggable handle=".handle">
+          <Container className={classes.windowBox}>
+            <AppBar className={`handle ${classes.handle}`} position="static">
+              <FontAwesomeIcon className={classes.icon} icon={faWindowClose} onClick={()=>close("HelloWorld")} />
+            </AppBar>
+            {children}
+          </Container>
+        </Draggable> )}
+    </ProcessConsumer>
+
   );
 };
 
