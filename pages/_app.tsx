7def24a9549ab4d559cc71210dad490a319656ed
jinseok9338 <jinseok9338@gmail.com>
2021-08-17 14:07:02 +0000

    Trying to make the os in another way

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,16 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import defaultTheme from "../styles/defaultTheme";
 import React from "react";
 import { SessionProvider } from "contexts/session";
-import { FileSystemProvider } from "contexts/fileSystem";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <FileSystemProvider>
+   
       <SessionProvider>
         <ThemeProvider theme={defaultTheme}>
           <Component {...pageProps} />
         </ThemeProvider>
       </SessionProvider>
-    </FileSystemProvider>
+   
   );
 }
 export default MyApp;
