f4bc93bbc821cb9394229e3de4cdc805729c23d6
jinseok9338 <jinseok9338@gmail.com>
2021-08-18 14:27:05 +0000

    tiny change

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -15,6 +15,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     overflow: hidden;
+    background-color: black;
   }
 
   ol,
