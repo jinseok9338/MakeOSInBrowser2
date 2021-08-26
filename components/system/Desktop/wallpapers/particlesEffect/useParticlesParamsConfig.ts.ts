83a3d2daf0420d1314a877320823fb7bf5ce3112
jinseok9338 <jinseok9338@gmail.com>
2021-08-26 13:31:45 +0000

    Making Props

---
+++ components/system/Desktop/wallpapers/particlesEffect/useParticlesParamsConfig.ts.ts
@@ -0,0 +1,17 @@
+import { IParticlesParams, ParticlesProps } from "react-particles-js";
+
+
+
+
+
+const useParticlesParamsConfig: ParticlesProps = (particleOptions: particleOptionsProps) => {
+    
+    const particleParamsConfig: ParticlesProps = {
+
+    };
+    
+    return particleParamsConfig;
+    
+}
+
+export default useParticlesParamsConfi;
