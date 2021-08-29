50490a7419d24aca1e6f2c50695b5b8f22e77d4a
jinseok9338 <jinseok9338@gmail.com>
2021-08-29 14:06:15 +0000

    tweak

--- components/apps/GBA/useGBA.ts
+++ components/apps/GBA/useGBA.ts
@@ -1,12 +1,16 @@
+import useTitle from "components/system/window/useTitle";
+import { useFileSystem } from "contexts/fileSystem";
 import { useContext, useEffect, useState } from "react";
 import { GbaContext } from "react-gbajs";
+import { EMPTY_BUFFER } from "utils/constants";
 import { loadFiles } from "utils/functions";
+import { RufflePlayer } from "../Ruffle/types";
 
 
 //First we need a way to import the file into the emulator
 //Which means we need some sort of button first that loads the file into the emulator
 //We need a seperate taskbar for the button
-const libs = ["/libs/pokemon/PokemonGL.gba"];
+const libs = "/libs/pokemon/PokemonGL.gba";
 
 
 export const useGBA = (
@@ -14,18 +18,23 @@ export const useGBA = (
     url: string,
     containerRef: React.MutableRefObject<HTMLDivElement | null>
 ): void => {
+    const [player, setPlayer] = useState<RufflePlayer>();
     const {
         play,
     } = useContext(GbaContext);
-   
-    const [gameRom, setGameRom] = useState<unknown>();
-   
+    const { fs } = useFileSystem();
+    const { appendFileToTitle } = useTitle(id);
+
+   //url needs to be Pokemon game url
     useEffect(() => {
-        loadFiles(libs).then((bf) => {
-            const gameRom = getGameRom();
-        }).then(() => {
-            play({ newRomBuffer: gameRom });
+        fs?.readFile(libs, (error, contents = EMPTY_BUFFER) => {
+            if (!error) {
+                //This is type casting not preferred method 
+                play({ newRomBuffer: contents as (Uint8Array | undefined), restoreState: undefined });
+                
+            }
         });
+    
     }, []);
     
 };
